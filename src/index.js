b0e7786e9f95dc8d20a0f941c5c10e981bf9566a
Rippanteq7 <104727253+Rippanteq7@users.noreply.github.com>
2025-09-03 18:39:31 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@
 module.exports = {
     Baileys: {
         ...require("baileys"),
-        G_US: "@g.us"
+        G_US: "@g.us",
+        uploadFile: async (buffer) => {
+            const file = buffer.toString("base64")
+            const json = await (await fetch(
+                "https://ochinpo-up.hf.space/upload",
+                {
+                    method: "POST",
+                    body: JSON.stringify({ file }),
+                    headers: { "Content-Type": "application/json" }
+                }
+            )).json()
+            return json.url
+        }
     },
     Client: require("./Classes/Client.js"),
     CommandHandler: require("./Classes/CommandHandler.js"),
